54a066c6c20c62709955744bd4c9727d690761cc
Emiliano Ayala <dissidia86@gmail.com>
2021-10-07 21:30:08 -0300

    NavBar (corregido)

--- src/Routers/AppRouter.js
+++ src/Routers/AppRouter.js
@@ -1,6 +1,17 @@
 import React from "react";
 import {BrowserRouter as Router} from "react-router-dom";
 import { NavBar } from "../common/components/NavBar";
+import {
+  Switch,
+  Route
+} from "react-router-dom";
+import { AltaEmpleado } from "../pages/AltaEmpleado/AltaEmpleado";
+import { AltatipoJornada } from "../pages/AltaTipoJornada/AltatipoJornada";
+import { CargaJornada } from "../pages/CargaJornada/CargaJornada";
+import { VerEmpleado } from "../pages/VerEmpleado/VerEmpleado";
+import { VerTurnos } from "../pages/VerTurnos/VerTurnos";
+import { Home } from "../Home";
+
 
 
 export default function AppRouter() {
@@ -12,6 +23,27 @@ export default function AppRouter() {
 
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
+
+<Switch>
+          <Route exact path="/AltaEmpleado">
+            <AltaEmpleado />
+          </Route>
+          <Route exact path="/AltaTipoJornada">
+            <AltatipoJornada />
+          </Route>
+          <Route exact path="/CargaJornada">
+            <CargaJornada />
+          </Route>
+          <Route exact path="/VerEmpleado">
+            <VerEmpleado />
+          </Route>
+          <Route exact path="/VerTurnos">
+            <VerTurnos />
+          </Route>
+          <Route exact path="/">
+            <Home />
+          </Route>
+        </Switch>
     
      
     </Router>
