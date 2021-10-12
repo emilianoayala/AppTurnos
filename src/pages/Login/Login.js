d78ba0a2553be2dc294db52563a55acab0e7a818
Emiliano Ayala <dissidia86@gmail.com>
2021-10-12 20:03:59 -0300

    PeticionesHttpHandlelogin

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,39 +1,47 @@
 import React, { useContext, useState } from "react";
 import { AuthContext } from "../../auth/AuthContext";
+import { fetchSinToken } from "../../common/helpers/fetch";
 import { types } from "../../types/types";
 import "./Login.css";
 
-export const Login = ( {history} ) => {
-
-  const {dispatch} = useContext(AuthContext);
+export const Login = ({ history }) => {
+  const { dispatch } = useContext(AuthContext);
 
   const [userName, setuserName] = useState();
 
   const [password, setPassword] = useState();
 
-  const handleLogin = ( e ) => {
-
+  const handleLogin = async (e) => {
     e.preventDefault();
-    console.log(userName, password)
-    history.replace("/");
-    dispatch({
-      type: types.login,
-      payload: {
-        userName: userName ,
-        isAdmin: true,
-        logged: true
-      }
-    })
-  }
-
-
+    const respuesta = await fetchSinToken(
+      "auth/login",
+      { userName, password },
+      "POST"
+    );
+    const body = await respuesta.json();
+    if (body.ok) {
+      const user = {
+        logged: true,
+        uid: body.uid,
+        userName: userName,
+        isAdmin: body.isAdmin,
+        email: body.email,
+        token: body.token,
+      };
+      localStorage.setItem("user", JSON.stringify(user));
+      history.replace("/");
+      dispatch({
+        type: types.login,
+        payload: user,
+      });
+    }s
+  };
 
   return (
     <div className="container">
       <div className="row">
         <div className="col-lg-3 col-md-2"></div>
         <div className="col-lg-6 col-md-8 login-box">
-          
           <div className="col-lg-12 login-title">Log In</div>
 
           <div className="col-lg-12 login-form">
@@ -41,13 +49,19 @@ export const Login = ( {history} ) => {
               <form>
                 <div className="form-group">
                   <label className="form-control-label">USERNAME</label>
-                  <input type="text" className="form-control" 
-                  onChange={(event)=>setuserName(event.target.value)}></input>
+                  <input
+                    type="text"
+                    className="form-control"
+                    onChange={(event) => setuserName(event.target.value)}
+                  ></input>
                 </div>
                 <div className="form-group">
                   <label className="form-control-label">PASSWORD</label>
-                  <input type="password" className="form-control" 
-                  onChange={(event)=>setPassword(event.target.value)}></input>
+                  <input
+                    type="password"
+                    className="form-control"
+                    onChange={(event) => setPassword(event.target.value)}
+                  ></input>
                 </div>
 
                 <div className="col-lg-12 loginbttm">
@@ -55,7 +69,10 @@ export const Login = ( {history} ) => {
                     {/* <!-- Error Message --> */}
                   </div>
                   <div className="col-lg-6 login-btm login-button">
-                    <button className="btn btn-outline-primary" onClick={ handleLogin }>
+                    <button
+                      className="btn btn-outline-primary"
+                      onClick={handleLogin}
+                    >
                       LOGIN
                     </button>
                   </div>
